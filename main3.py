19de1d5c60514ccf8b59470fdbc5ba11315a6b35
nasrin pathan <79505893+nasrin1748@users.noreply.github.com>
2024-12-05 13:57:31 +0000

    Create main3.py

---
+++ main3.py
@@ -0,0 +1,9 @@
+def fun():
+    return "hello" 
+
+def fun1():
+    return '<span><div style="color: green; font-size: 20px; font-weight: bold; border: 2px solid #007bff; padding: 10px; border-radius: 5px; background-color: #ffffff;">Hello from Python with inline styling!</div></span>'
+
+def greet3():
+    return fun()+fun1()+fun()+fun1()
+
