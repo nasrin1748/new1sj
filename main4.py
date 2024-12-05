ed5d4585d1b9520c45a4f2436332d383ef41d9b5
nasrin pathan <79505893+nasrin1748@users.noreply.github.com>
2024-12-05 13:57:57 +0000

    Create main4.py

---
+++ main4.py
@@ -0,0 +1,15 @@
+def greet4():
+    html_content = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <p>hello World</p>
+        <h1>hello</h1>
+    </body>
+    </html>
+    """
+    return html_content
