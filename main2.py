ed7892401573ad5f4185c6385b8d0ce144c8ccc9
nasrin pathan <79505893+nasrin1748@users.noreply.github.com>
2024-12-05 13:57:00 +0000

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,4 @@
+from pyscript import display
+def greet2():
+    return '<span><div style="color: blue; font-size: 20px; font-weight: bold; border: 2px solid #007bff; padding: 10px; border-radius: 5px; background-color: #e7f3ff;">Hello from Python with inline styling!</div></span>'
+
